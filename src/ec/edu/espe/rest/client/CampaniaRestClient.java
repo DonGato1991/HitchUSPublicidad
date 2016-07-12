fb0daf1d8df37e3235c71925f7ec440dab229606
Juan Caspi <juanitocaspi@gmail.com>
2016-07-12 14:07:34 -0500

    Merge origin/master
    Conflicts:
    	src/ec/edu/espe/rest/client/CampaniaRestClient.java
    	src/ec/edu/espe/rest/client/DetalleCampaniaRestClient.java
    	src/ec/edu/espe/rest/client/DetalleFacturaRestClient.java
    	src/ec/edu/espe/rest/client/ElementoRestClient.java
    	src/ec/edu/espe/rest/client/EmpresaRestClient.java
    	src/ec/edu/espe/rest/client/EstadisticaCampaniaRestClient.java
    	src/ec/edu/espe/rest/client/FacturaEmpresaRestClient.java
    	src/ec/edu/espe/rest/client/HistorialCampaniaRestClient.java
    	src/ec/edu/espe/rest/client/SegmentoDetalleCampaniaRestClient.java
    	src/ec/edu/espe/rest/client/TargetEdadRestClient.java
    	src/ec/edu/espe/rest/client/UsuarioRestClient.java

--- src/ec/edu/espe/rest/client/CampaniaRestClient.java
+++ src/ec/edu/espe/rest/client/CampaniaRestClient.java
@@ -27,7 +27,7 @@ public class CampaniaRestClient {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:8080/PublicidadWebHitchUs/webresources";
+    private static final String BASE_URI = "http://52.41.82.209:4848/PublicidadWebHitchUs/webresources";
 
     public CampaniaRestClient() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
